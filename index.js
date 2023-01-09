618a7a7562197b1f54db878bcc756134f7938611
Eliza Chernysh <82468286+ElizaChernysh@users.noreply.github.com>
2023-01-09 15:54:16 +0200

    fix 09-2

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const upload = multer({ storage });
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
