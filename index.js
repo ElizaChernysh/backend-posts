e2a80daaafe78ab1c1f7b8418a70e41afd4ee576
Eliza Chernysh <82468286+ElizaChernysh@users.noreply.github.com>
2023-01-11 15:39:54 +0200

    change

--- index.js
+++ index.js
@@ -13,74 +13,69 @@ dotenv.config();
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('DB Ok'))
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+  destination: (_, __, cb) => {
     if (!fs.existsSync('uploads')) {
       fs.mkdirSync('uploads');
     }
-    cb(null, './uploads');
+    cb(null, 'uploads');
   },
-  filename: (req, file, cb) => {
+  filename: (_, file, cb) => {
     cb(null, file.originalname);
-  }
+  },
 });
 
 const upload = multer({ storage });
 
 app.use(express.json());
-
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
+
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-app.post('/auth/register', registerValidation, handleValidationErrors,register);
-app.post('/auth/login',loginValidation, handleValidationErrors, login);
+app.post('/auth/login', loginValidation, handleValidationErrors, login);
+app.post('/auth/register', registerValidation, handleValidationErrors, register);
 app.get('/auth/me', checkAuth, getMe);
 
-// app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-//   const file = req.file;
-//   if (!file) {
-//     const error = new Error('Please upload a file')
-//     return error;
-//   }
-//     res.send(file);
- 
-// });
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
 
-app.post('/upload', checkAuth, upload.single('image'), (req, res, next) => {
-  console.log(JSON.stringify(req.file));
-
-  const file = req.file.path;
-  if (!file) {
-    const error = new Error('Please upload a file')
-    return error;
-  }
-    res.send(file);
+  res.send({
+    url: `/uploads/${req.file.originalname}`,
+  })
 });
 
 app.get('/tags', getLastTags);
+
 app.get('/posts', getAll);
 app.get('/posts/tags', getLastTags);
 app.get('/posts/:id', getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, create);
 app.delete('/posts/:id', checkAuth, remove);
-app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, update);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  update,
+);
 
-
-app.listen(process.env.PORT || '4444', (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
 
-  console.log('Server Ok')
+  console.log('Server OK');
 });
-
